e03898f25936546b73dda09ab386ce7847b11d07
buuoltwo <bullnick20@gmail.com>
2025-03-22 20:11:19 +0800

    name == null || name.isEmpty()

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.encapsulation;
 
+import com.sun.tools.javac.util.StringUtils;
+
+import java.util.Arrays;
+
 public class Cat {
     private static final Cat INVALID_CAT = new Cat("Invalid cat", -1);
     private String name;
@@ -18,7 +22,17 @@ public class Cat {
      * @param name 名字
      * @return 创建的猫
      */
-    public static Cat newCat(String name, int age) {}
+    public static Cat newCat(String name, int age) {
+        if(age < 0 || (name == null || name.isEmpty())) {
+            return INVALID_CAT;
+        }
+        return new Cat(name, age);
+    }
+
+    private Cat( String name,int age) {
+        this.age = age;
+        this.name = name;
+    }
 
     public String getName() {
         return name;
